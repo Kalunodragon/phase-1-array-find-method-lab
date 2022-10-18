f0a3745628a7b91e2768608f06cfb5471fc3fe87
Andrew <andrewonulak@gmail.com>
2022-10-17 18:20:47 -0600

    commit tree 57fa6838047f052592c79fa9f2d3424eab2338c3

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+/*
+function superbowlWin(array){
+    for (const object of array) {
+      if (object.result === "W") {
+        return (object.year);
+        }
+    }
+}
+*/
+
+function superbowlWin(array){
+    let object = array.find(array => array.result === "W")
+    if (object){
+        return object.year
+    }
+    return object
+}
